97bd5cf5a4ca3f9ee1c7adb7fee4c3543c265db7
Robert von Burg <eitch@eitchnet.ch>
2012-06-14 23:38:50 +0200

    [New] Added packaging information and files and licence
    This includes setting the licence to LGPL, setting the headers, adding a build script and testing the build

--- src/ch/eitchnet/rmi/RmiHelper.java
+++ src/ch/eitchnet/rmi/RmiHelper.java
@@ -1,11 +1,25 @@
 /*
- * Copyright (c) 2010 - 2011
+ * Copyright (c) 2012
  * 
- * Apixxo AG 
- * Hauptgasse 25
- * 4600 Olten
+ * Robert von Burg <eitch@eitchnet.ch>
  * 
- * All rights reserved.
+ */
+
+/*
+ * This file is part of ch.eitchnet.java.utils
+ *
+ * Privilege is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Privilege is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Privilege.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 package ch.eitchnet.rmi;
--- src/ch/eitchnet/utils/objectfilter/ObjectCache.java
+++ src/ch/eitchnet/utils/objectfilter/ObjectCache.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2012
+ * 
+ * Michael Gatto <michael@gatto.ch>
+ * 
+ */
+
+/*
+ * This file is part of ch.eitchnet.java.utils
+ *
+ * Privilege is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Privilege is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Privilege.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package ch.eitchnet.utils.objectfilter;
 
 import org.apache.log4j.Logger;
