d79532c2015f3abfa14bc9462d34a9bd7552d184
Robert von Burg <eitch@eitchnet.ch>
2013-11-14 21:42:48 +0100

    [New] Added new script to list git status of each project

---
+++ status.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#######################################################################
+##
+## Show status of all projects
+##
+#######################################################################
+
+echo "Showing status of li.strolch.dev..." ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.parent..." ; cd li.strolch.parent ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.bom..." ; cd li.strolch.bom ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.model..." ; cd li.strolch.model ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.testbase..." ; cd li.strolch.testbase ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.runtime..." ; cd li.strolch.runtime ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.service..." ; cd li.strolch.service ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.persistence.api..." ; cd li.strolch.persistence.api ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.persistence.xml..." ; cd li.strolch.persistence.xml ; git status -s ; cd .. ; echo
+echo "Showing status of li.strolch.tutorialapp..." ; cd li.strolch.tutorialapp ; git status -s ; cd .. ; echo
+
+echo "Done."
+exit 0
