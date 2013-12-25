0879db11526cac73072924464cc5e55f6ba40738
Robert von Burg <eitch@eitchnet.ch>
2013-12-25 17:33:55 +0100

    [Minor] refactored tests for persistence layers to remove duplicate code

---
+++ src/main/java/li/strolch/testbase/runtime/AbstractModelTest.java
@@ -0,0 +1,59 @@
+package li.strolch.testbase.runtime;
+
+import li.strolch.persistence.api.StrolchTransaction;
+
+import org.junit.Test;
+
+public abstract class AbstractModelTest {
+
+	protected abstract RuntimeMock getRuntimeMock();
+
+	@Test
+	public void shouldStartContainer() {
+		try (StrolchTransaction tx = getRuntimeMock().getPersistenceHandler().openTx()) {
+			tx.getOrderDao().queryKeySet();
+		}
+	}
+
+	@Test
+	public void shouldCreateOrders() {
+
+		OrderModelTestRunner testRunner = new OrderModelTestRunner(getRuntimeMock());
+		testRunner.runCreateOrderTest();
+	}
+
+	@Test
+	public void shouldOrderCrud() {
+
+		OrderModelTestRunner testRunner = new OrderModelTestRunner(getRuntimeMock());
+		testRunner.runCrudTests();
+	}
+
+	@Test
+	public void shouldOrderPerformBulkOperations() {
+
+		OrderModelTestRunner testRunner = new OrderModelTestRunner(getRuntimeMock());
+		testRunner.runBulkOperationTests();
+	}
+
+	@Test
+	public void shouldCreateResources() {
+
+		ResourceModelTestRunner testRunner = new ResourceModelTestRunner(getRuntimeMock());
+		testRunner.runCreateResourceTest();
+	}
+
+	@Test
+	public void shouldResourceCrud() {
+
+		ResourceModelTestRunner testRunner = new ResourceModelTestRunner(getRuntimeMock());
+		testRunner.runCrudTests();
+	}
+
+	@Test
+	public void shouldResourcePerformBulkOperations() {
+
+		ResourceModelTestRunner testRunner = new ResourceModelTestRunner(getRuntimeMock());
+		testRunner.runBulkOperationTests();
+	}
+}
