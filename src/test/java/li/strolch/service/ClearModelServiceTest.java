29ca8ba3c5d5d4d93ebd2567d9e326b7198ce6c9
Robert von Burg <eitch@eitchnet.ch>
2014-07-31 16:34:58 +0200

    [New] Added ClearModelServiceTest

---
+++ src/test/java/li/strolch/service/ClearModelServiceTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013 Robert von Burg <eitch@eitchnet.ch>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package li.strolch.service;
+
+import li.strolch.service.api.Service;
+import li.strolch.service.api.ServiceArgument;
+import li.strolch.service.api.ServiceResult;
+import li.strolch.service.test.AbstractRealmServiceTest;
+
+import org.junit.Before;
+
+/**
+ * @author Robert von Burg <eitch@eitchnet.ch>
+ */
+public class ClearModelServiceTest extends AbstractRealmServiceTest {
+
+	private ClearModelService svc;
+	private ClearModelArgument arg;
+
+	@Before
+	public void before() {
+		svc = new ClearModelService();
+		arg = new ClearModelArgument();
+		arg.clearOrders = true;
+		arg.clearResources = true;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T extends ServiceArgument> T getArg() {
+		return (T) this.arg;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T extends ServiceArgument, U extends ServiceResult> Service<T, U> getSvc() {
+		return (Service<T, U>) this.svc;
+	}
+}
