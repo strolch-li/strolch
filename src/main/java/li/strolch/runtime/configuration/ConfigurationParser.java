3c05496cebb19ca91b938b798d01ecfc9f2e70ad
Robert von Burg <eitch@eitchnet.ch>
2014-08-24 10:22:22 +0200

    [Minor] code cleanup - extracted constants for configuration - fixed redundant null check - cleaned up compiler warnings on i18n

--- src/main/java/li/strolch/runtime/configuration/ConfigurationParser.java
+++ src/main/java/li/strolch/runtime/configuration/ConfigurationParser.java
@@ -18,7 +18,6 @@ package li.strolch.runtime.configuration;
 import java.io.File;
 import java.text.MessageFormat;
 
-import li.strolch.model.Tags;
 import li.strolch.runtime.configuration.ConfigurationSaxParser.ConfigurationBuilder;
 import ch.eitchnet.utils.dbc.DBC;
 import ch.eitchnet.utils.helper.XmlHelper;
@@ -32,7 +31,7 @@ public class ConfigurationParser {
 	public static StrolchConfiguration parseConfiguration(String environment, File rootPathF) {
 		DBC.PRE.assertNotEmpty("environment value must be set!", environment); //$NON-NLS-1$
 		DBC.PRE.assertNotNull("roothPath must be set!", rootPathF); //$NON-NLS-1$
-		DBC.PRE.assertNotEquals("environment must be a value other than 'global'!", Tags.GLOBAL, environment); //$NON-NLS-1$
+		DBC.PRE.assertNotEquals("environment must be a value other than 'global'!", ConfigurationTags.ENV_GLOBAL, environment); //$NON-NLS-1$
 
 		if (!rootPathF.isDirectory() || !rootPathF.canRead()) {
 			String msg = "Root path is not readable at {0}"; //$NON-NLS-1$
