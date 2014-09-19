ebc549dc711851802d8024ed25f2f4734c0ed55c
Robert von Burg <eitch@eitchnet.ch>
2014-09-20 00:14:12 +0200

    [New] added new setVersion.sh script

--- setVersion.sh
+++ setVersion.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
-if [ "$#" != "1" ] ; then
-    echo "ERROR: Version missing or wrong arguments!"
+if [ "$#" != "2" ] ; then
+    echo "ERROR: Wrong arguments!"
+    echo "Usage: $0 <old_version> <new_version>"
     exit 1
 fi
 
-if ! mvn -f pom.xml versions:set -DnewVersion="${1}" -DallowSnapshots=true -DgenerateBackupPoms=false ; then
-    echo "ERROR: Failed to change version in root!"
-    exit 1
-fi
-if ! mvn -f versioning_eitchnet_pom.xml versions:set -DnewVersion="${1}" -DallowSnapshots=true -DgenerateBackupPoms=false ; then
-    echo "ERROR: Failed to change version in submodule!"
-    exit 1
-fi
+#if ! mvn -f pom.xml versions:set -DnewVersion="${1}" -DallowSnapshots=true -DgenerateBackupPoms=false ; then
+#    echo "ERROR: Failed to change version in root!"
+#    exit 1
+#fi
+#if ! mvn -f versioning_eitchnet_pom.xml versions:set -DnewVersion="${1}" -DallowSnapshots=true -DgenerateBackupPoms=false ; then
+#    echo "ERROR: Failed to change version in submodule!"
+#    exit 1
+#fi
 
+old_version="${$1}"
+new_version="${$2}"
+
+root="${PWD}"
+
+xmlstarlet ed --ps -L -N my=http://maven.apache.org/POM/4.0.0 -u "/my:project/my:version" -v ${new_version} pom.xml
+xmlstarlet ed --ps -L -N my=http://maven.apache.org/POM/4.0.0 -u "/my:project/my:parent/my:version" -v ${new_version} pom.xml
+for project in li.* ; then
+  cd "${root}/${project}"
+  xmlstarlet ed --ps -L -N my=http://maven.apache.org/POM/4.0.0 -u "/my:project/my:version" -v ${new_version} pom.xml
+  xmlstarlet ed --ps -L -N my=http://maven.apache.org/POM/4.0.0 -u "/my:project/my:parent/my:version" -v ${new_version} pom.xml
+done
+for project in ch.* ; then
+  cd "${root}/${project}"
+  xmlstarlet ed --ps -L -N my=http://maven.apache.org/POM/4.0.0 -u "/my:project/my:version" -v ${new_version} pom.xml
+  xmlstarlet ed --ps -L -N my=http://maven.apache.org/POM/4.0.0 -u "/my:project/my:parent/my:version" -v ${new_version} pom.xml
+done
+
+sed --in-place "s/${old_version}/${new_version}/" li.strolch.dev/createBundle.sh
