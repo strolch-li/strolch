a78fd702bdf23143c279005f1ec26ed058d6ca5e
Robert von Burg <eitch@eitchnet.ch>
2014-09-19 22:36:59 +0200

    [New] added new setVersion.sh script

--- setVersion.sh
+++ setVersion.sh
@@ -3,4 +3,13 @@ if [ "$#" != "1" ] ; then
     echo "ERROR: Version missing or wrong arguments!"
     exit 1
 fi
-mvn versions:set -DnewVersion="${1}" -DallowSnapshots=true -DgenerateBackupPoms=false
+
+if ! mvn -f pom.xml versions:set -DnewVersion="${1}" -DallowSnapshots=true -DgenerateBackupPoms=false ; then
+    echo "ERROR: Failed to change version in root!"
+    exit 1
+fi
+if ! mvn -f versioning_eitchnet_pom.xml versions:set -DnewVersion="${1}" -DallowSnapshots=true -DgenerateBackupPoms=false ; then
+    echo "ERROR: Failed to change version in submodule!"
+    exit 1
+fi
+
