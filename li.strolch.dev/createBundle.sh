7c0ab16c9e257fc69ed9dacc4c98b8628b7a9f65
Robert von Burg <eitch@eitchnet.ch>
2014-09-20 14:38:29 +0200

    [Project] bumped version from 1.0.0-SNAPSHOT to 1.0.0-RC1

--- li.strolch.dev/createBundle.sh
+++ li.strolch.dev/createBundle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 projectName=strolch_bundle
-projectVersion=1.0.0-SNAPSHOT
+projectVersion=1.0.0-RC1
 DIST_STROLCH="/var/www/eitch/www.strolch.li/dist/snapshot"
 DEPLOY_SERVER="appsrv.gsi.local"
 ROOT="${PWD}"
