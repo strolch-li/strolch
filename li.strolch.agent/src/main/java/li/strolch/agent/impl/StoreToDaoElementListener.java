48caa84903eb6beb20c33a1aef7be4c8f87e5d90
Robert von Burg <eitch@eitchnet.ch>
2016-09-29 15:54:30 +0200

    [Major] Refactored SystemAction in privileges
    Simplified the API, removed the privileged user - now always use the
    agent system user for running system actions. One method has no return
    value and one has a return value. Now it is easy to perform a system
    action using:
    
        runAsAgent(ctx -> {
          // do work			
        });
    
        String result = runAsAgentWithResult(ctx -> {
          // do work
          return "done";
        });
    
        // execute a SystemAction
        runAsAgent(action);
    
        // execute a SystemActionWithResult
        String result = runAsAgentWithResult(actionWithResult);

--- li.strolch.agent/src/main/java/li/strolch/agent/impl/StoreToDaoElementListener.java
+++ li.strolch.agent/src/main/java/li/strolch/agent/impl/StoreToDaoElementListener.java
@@ -41,21 +41,21 @@ public class StoreToDaoElementListener implements StrolchElementListener {
 
 	@Override
 	public void notifyResource(Resource resource) {
-		if (tx.isVersioningEnabled() && resource.getVersion() == null)
+		if (resource.getVersion() == null)
 			Version.setInitialVersionFor(resource, tx.getCertificate().getUsername());
 		this.resourceDao.save(resource);
 	}
 
 	@Override
 	public void notifyOrder(Order order) {
-		if (tx.isVersioningEnabled() && order.getVersion() == null)
+		if (order.getVersion() == null)
 			Version.setInitialVersionFor(order, tx.getCertificate().getUsername());
 		this.orderDao.save(order);
 	}
 
 	@Override
 	public void notifyActivity(Activity activity) {
-		if (tx.isVersioningEnabled() && activity.getVersion() == null)
+		if (activity.getVersion() == null)
 			Version.setInitialVersionFor(activity, tx.getCertificate().getUsername());
 		this.activityDao.save(activity);
 	}
