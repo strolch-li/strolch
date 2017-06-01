2470b02a103a1d864b353d60a4113644ef7f3a96
Robert von Burg <eitch@eitchnet.ch>
2017-06-01 12:41:03 +0200

    [New] Added StringServiceArgument

---
+++ li.strolch.service/src/main/java/li/strolch/service/StringServiceArgument.java
@@ -0,0 +1,8 @@
+package li.strolch.service;
+
+import li.strolch.service.api.ServiceArgument;
+
+public class StringServiceArgument extends ServiceArgument {
+	private static final long serialVersionUID = 1L;
+	public String value;
+}
