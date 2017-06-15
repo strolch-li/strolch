030ff9f6b351fce73e01646650d04db3758b5484
Robert von Burg <eitch@eitchnet.ch>
2017-06-15 13:33:24 +0200

    [New] Added FixedSizeQueue

---
+++ li.strolch.utils/src/main/java/li/strolch/utils/collections/FixedSizeQueue.java
@@ -0,0 +1,33 @@
+package li.strolch.utils.collections;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class FixedSizeQueue<E> extends LinkedList<E> {
+
+	private static final long serialVersionUID = 1L;
+
+	private int maxSize;
+
+	public FixedSizeQueue(int size) {
+		this.maxSize = size;
+	}
+
+	@Override
+	public boolean add(E e) {
+		boolean b = super.add(e);
+		if (size() >= this.maxSize) {
+			removeRange(0, size() - this.maxSize);
+		}
+		return b;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		boolean b = super.addAll(index, c);
+		if (size() >= this.maxSize) {
+			removeRange(0, size() - this.maxSize);
+		}
+		return b;
+	}
+}
