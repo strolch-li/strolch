0c6f62e31e261e01ee26ce509f9547e357c979b5
Robert von Burg <eitch@eitchnet.ch>
2017-08-10 11:54:36 +0200

    [New] Added NetworkHelper.findInet4Addresses()

---
+++ li.strolch.utils/src/main/java/li/strolch/utils/helper/NetworkHelper.java
@@ -0,0 +1,45 @@
+package li.strolch.utils.helper;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class NetworkHelper {
+
+	public static List<Inet4Address> findInet4Addresses() throws SocketException {
+
+		List<Inet4Address> inet4Addresses = new ArrayList<>();
+
+		Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+		while (interfaces.hasMoreElements()) {
+			NetworkInterface ni = interfaces.nextElement();
+
+			if (ni.isLoopback() || ni.isPointToPoint() || ni.isVirtual())
+				continue;
+
+			Enumeration<InetAddress> inetAddresses = ni.getInetAddresses();
+			while (inetAddresses.hasMoreElements()) {
+				InetAddress ia = inetAddresses.nextElement();
+
+				if (ia instanceof Inet4Address)
+					inet4Addresses.add((Inet4Address) ia);
+			}
+		}
+
+		return inet4Addresses;
+	}
+
+	public static String formatMacAddress(byte[] bytes) {
+		StringBuilder sb = new StringBuilder(17);
+		for (byte b : bytes) {
+			if (sb.length() > 0)
+				sb.append(':');
+			sb.append(String.format("%02x", b));
+		}
+		return sb.toString();
+	}
+}
