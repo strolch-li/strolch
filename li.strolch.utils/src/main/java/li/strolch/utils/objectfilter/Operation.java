475a3b2c1ca344dfd63e24d7c8d4b89d2f63a1e7
Robert von Burg <eitch@eitchnet.ch>
2018-03-08 18:05:01 +0100

    [Minor] Comment fix in Operation.java

--- li.strolch.utils/src/main/java/li/strolch/utils/objectfilter/Operation.java
+++ li.strolch.utils/src/main/java/li/strolch/utils/objectfilter/Operation.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Michael Gatto <michael@gatto.ch>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,20 +17,23 @@ package li.strolch.utils.objectfilter;
 
 /**
  * A discrete set of operations associated to some object / state
- * 
+ *
  * @author Michael Gatto &lt;michael@gatto.ch&gt;
  */
 public enum Operation {
+
 	/**
-	 * ADD The operation associated to something is addition.
+	 * ADD The operation associated when an object was added/created
 	 */
 	ADD,
+
 	/**
-	 * MODIFY The operation associated to something is a modification.
+	 * MODIFY The operation associated when an object was modified
 	 */
 	MODIFY,
+
 	/**
-	 * REMOVE The operation associated to something is removal
+	 * REMOVE The operation associated when an object was removed
 	 */
 	REMOVE;
 }
