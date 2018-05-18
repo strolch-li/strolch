f8e9c5321c7937578024573f2571522c55ecc24b
Robert von Burg <eitch@eitchnet.ch>
2018-05-18 08:26:50 +0200

    [Minor] Better logging in DefaultLockHandler

--- li.strolch.agent/src/main/java/li/strolch/agent/impl/DefaultLockHandler.java
+++ li.strolch.agent/src/main/java/li/strolch/agent/impl/DefaultLockHandler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Robert von Burg <eitch@eitchnet.ch>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -105,7 +105,9 @@ public class DefaultLockHandler implements LockHandler {
 			// logger.debug("locked " + locator); //$NON-NLS-1$
 
 		} catch (InterruptedException e) {
-			throw new StrolchLockException(e.getMessage(), e); //$NON-NLS-1$
+			String msg = "Interrupted while trying to acquire lock for {1}"; //$NON-NLS-1$
+			msg = MessageFormat.format(msg, locator);
+			throw new StrolchLockException(msg, e);
 		}
 	}
 
@@ -116,7 +118,8 @@ public class DefaultLockHandler implements LockHandler {
 		try {
 			lock.unlock();
 		} catch (IllegalMonitorStateException e) {
-			throw new StrolchLockException(e.getMessage(), e); //$NON-NLS-1$
+			throw new StrolchLockException("IllegalMonitorStateException when trying to unlock: " + e.getMessage(),
+					e); //$NON-NLS-1$
 		}
 	}
 
