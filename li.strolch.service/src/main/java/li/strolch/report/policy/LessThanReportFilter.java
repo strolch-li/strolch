466838d73b6b29d4de62f76c35c61c989c22cfbc
Robert von Burg <eitch@eitchnet.ch>
2018-06-27 11:12:46 +0200

    [New] Implemented LessThan and now(Period) filter in reports

---
+++ li.strolch.service/src/main/java/li/strolch/report/policy/LessThanReportFilter.java
@@ -0,0 +1,20 @@
+package li.strolch.report.policy;
+
+import li.strolch.agent.api.ComponentContainer;
+import li.strolch.persistence.api.StrolchTransaction;
+import li.strolch.utils.ObjectHelper;
+
+public class LessThanReportFilter extends ReportFilterPolicy {
+
+	public LessThanReportFilter(ComponentContainer container, StrolchTransaction tx) {
+		super(container, tx);
+	}
+
+	@Override
+	protected boolean filter(Object left, Object right, boolean negate) {
+		if (negate)
+			return ObjectHelper.compare(left, right, false) < 0;
+		else
+			return ObjectHelper.compare(right, left, false) < 0;
+	}
+}
--- li.strolch.service/src/main/java/li/strolch/report/policy/ReportFilterPolicy.java
+++ li.strolch.service/src/main/java/li/strolch/report/policy/ReportFilterPolicy.java
@@ -1,11 +1,16 @@
 package li.strolch.report.policy;
 
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Date;
 
 import li.strolch.agent.api.ComponentContainer;
+import li.strolch.model.StrolchValueType;
 import li.strolch.model.parameter.Parameter;
 import li.strolch.persistence.api.StrolchTransaction;
 import li.strolch.policy.StrolchPolicy;
+import li.strolch.utils.dbc.DBC;
 import li.strolch.utils.iso8601.ISO8601FormatFactory;
 
 public abstract class ReportFilterPolicy extends StrolchPolicy {
@@ -53,23 +58,49 @@ public abstract class ReportFilterPolicy extends StrolchPolicy {
 		if (value instanceof Date) {
 
 			if (this.left == null)
-				this.left = ISO8601FormatFactory.getInstance().parseDate(this.filterValue);
+				this.left = parseFilterValueToDate(this.filterValue);
+
 			right = value;
 
 		} else if (value instanceof Parameter) {
 
 			Parameter parameter = (Parameter) value;
-			if (this.left == null)
-				this.left = parameter.getValueType().parseValue(this.filterValue);
+			if (this.left == null) {
+				StrolchValueType valueType = parameter.getValueType();
+				if (valueType == StrolchValueType.DATE)
+					this.left = parseFilterValueToDate(this.filterValue);
+				else
+					this.left = valueType.parseValue(this.filterValue);
+			}
+
 			right = parameter.getValue();
 
 		} else {
 			if (this.left == null)
 				this.left = this.filterValue;
+
 			right = value.toString();
 		}
 
 		return filter(this.left, right, this.negate);
+	}
+
+	private Date parseFilterValueToDate(String filterValue) {
+		DBC.INTERIM.assertNotEmpty("filterValue must not be empty for date comparisons!", filterValue);
+
+		if (!filterValue.startsWith("now"))
+			return ISO8601FormatFactory.getInstance().parseDate(filterValue);
+
+		if (filterValue.charAt(3) != '(' || filterValue.charAt(filterValue.length() - 1) != ')')
+			throw new IllegalArgumentException("now() format invalid for " + filterValue);
+
+		String periodS = filterValue.substring(4, filterValue.length() - 1);
+		if (periodS.isEmpty())
+			return new Date();
+
+		Period period = Period.parse(periodS);
+		LocalDateTime dateTime = LocalDateTime.now().plus(period);
+		return Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
 	}
 
 	protected abstract boolean filter(Object left, Object right, boolean negate);
