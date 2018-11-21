a93f3fc2218dc7c8e0d9d428822f7430d714c1df
Robert von Burg <eitch@eitchnet.ch>
2018-11-21 12:47:13 +0100

    [New] Added TypedTuple

---
+++ li.strolch.utils/src/main/java/li/strolch/utils/collections/TypedTuple.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2013 Robert von Burg <eitch@eitchnet.ch>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package li.strolch.utils.collections;
+
+/**
+ *
+ * Simple wrapper for two elements
+ *
+ * @author Robert von Burg &lt;eitch@eitchnet.ch&gt;
+ *
+ * @param <T> first object
+ * @param <U> second object
+ */
+public class TypedTuple<T, U> {
+
+	private T first;
+	private U second;
+
+	public TypedTuple() {
+		//
+	}
+
+	public TypedTuple(T first, U second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public T getFirst() {
+		return (T) this.first;
+	}
+
+	public void setFirst(T first) {
+		this.first = first;
+	}
+
+	public U getSecond() {
+		return (U) this.second;
+	}
+
+	public void setSecond(U second) {
+		this.second = second;
+	}
+}
