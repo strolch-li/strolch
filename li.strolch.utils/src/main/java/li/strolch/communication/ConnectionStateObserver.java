961e93fa352681952a640162aa440bfd5a437bdd
Robert von Burg <eitch@eitchnet.ch>
2019-04-05 15:22:22 +0200

    [Major] Removed connection from utils (use Apache Camel)

--- li.strolch.utils/src/main/java/li/strolch/communication/ConnectionStateObserver.java
+++
@@ -1,6 +0,0 @@
-package li.strolch.communication;
-
-public interface ConnectionStateObserver {
-
-	public void notify(ConnectionState oldState, String oldStateMsg, ConnectionState newState, String newStateMsg);
-}
