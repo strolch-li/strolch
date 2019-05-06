251d0cab1fdc3daebd7850ca127bf53472689b15
Robert von Burg <eitch@eitchnet.ch>
2019-05-06 14:49:24 +0200

    [New] Added new StringListArgument

---
+++ li.strolch.service/src/main/java/li/strolch/service/StringListArgument.java
@@ -0,0 +1,10 @@
+package li.strolch.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import li.strolch.service.api.ServiceArgument;
+
+public class StringListArgument extends ServiceArgument {
+	public List<String> list = new ArrayList<>();
+}
