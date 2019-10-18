4b1a6c9fec6afba5db8e43637b38e491393af5c2
Robert von Burg <eitch@eitchnet.ch>
2019-10-18 17:27:25 +0200

    [Minor] Added StrolchPolicy.getOrder(Action)

--- li.strolch.agent/src/main/java/li/strolch/policy/StrolchPolicy.java
+++ li.strolch.agent/src/main/java/li/strolch/policy/StrolchPolicy.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015 Robert von Burg <eitch@eitchnet.ch>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,8 +15,10 @@
  */
 package li.strolch.policy;
 
+import static li.strolch.runtime.StrolchConstants.PolicyConstants.PARAM_ORDER;
 import static li.strolch.utils.helper.StringHelper.DASH;
 
+import li.strolch.model.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -119,6 +121,10 @@ public abstract class StrolchPolicy {
 			throw new IllegalArgumentException("No resourceType defined on action " + action.getLocator());
 
 		return this.tx.getResourceBy(resourceType, resourceId, true);
+	}
+
+	protected Order getOrder(Action action) {
+		return tx().getOrderByRelation(action.getRootElement(), PARAM_ORDER, true);
 	}
 
 	/**
