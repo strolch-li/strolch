ddbb7d65cbdc81e731853c4dac4fcd3dea911c36
Robert von Burg <eitch@eitchnet.ch>
2020-01-08 09:11:45 +0100

    [New] Added overrides for ComponentConfiguration. getConfigFile() getDataDir() getDataFile()

--- li.strolch.agent/src/main/java/li/strolch/runtime/configuration/ComponentConfiguration.java
+++ li.strolch.agent/src/main/java/li/strolch/runtime/configuration/ComponentConfiguration.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Robert von Burg <eitch@eitchnet.ch>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,7 @@
  */
 package li.strolch.runtime.configuration;
 
+import java.io.File;
 import java.util.Map;
 import java.util.Set;
 
@@ -49,5 +50,17 @@ public class ComponentConfiguration extends AbstractionConfiguration {
 
 	public Set<String> getDependencies() {
 		return this.dependencies;
+	}
+
+	public File getConfigFile(String key, String defValue) {
+		return super.getConfigFile(key, defValue, getRuntimeConfiguration());
+	}
+
+	public File getDataDir(String key, String defValue, boolean checkExists) {
+		return super.getDataDir(key, defValue, getRuntimeConfiguration(), checkExists);
+	}
+
+	public File getDataFile(String key, String defValue, boolean checkExists) {
+		return super.getDataFile(key, defValue, getRuntimeConfiguration(), checkExists);
 	}
 }
