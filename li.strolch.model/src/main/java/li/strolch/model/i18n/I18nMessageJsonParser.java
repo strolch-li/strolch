af8ac81a18d83cd2458d63b8e5e837ba89db02fa
Robert von Burg <eitch@eitchnet.ch>
2020-05-15 17:24:47 +0200

    [WIP] Added I18nMessageJsonParser

---
+++ li.strolch.model/src/main/java/li/strolch/model/i18n/I18nMessageJsonParser.java
@@ -0,0 +1,24 @@
+package li.strolch.model.i18n;
+
+import com.google.gson.JsonObject;
+import li.strolch.utils.I18nMessage;
+
+public class I18nMessageJsonParser {
+
+	public static I18nMessage parse(JsonObject messageJ) {
+
+		// TODO
+
+//		String key = json.addProperty("key", message.getKey());
+//		json.addProperty("message", message.getMessage());
+//
+//		Properties values = message.getValues();
+//		if (!values.isEmpty()) {
+//			JsonObject valuesJ = new JsonObject();
+//			values.stringPropertyNames().forEach(key -> valuesJ.addProperty(key, values.getProperty(key)));
+//			json.add("values", valuesJ);
+//		}
+
+		return null;
+	}
+}
