f5ce31efe12c1b0efe3cc515919c3b8ac3ed1b3b
Robert von Burg <eitch@eitchnet.ch>
2021-12-13 12:16:55 +0100

    [New] TimeVariable returns NavigableSet instead of Collection

--- li.strolch.model/src/test/java/li/strolch/model/timedstate/TimeStateTest.java
+++ li.strolch.model/src/test/java/li/strolch/model/timedstate/TimeStateTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Martin Smock <smock.martin@gmail.com>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,29 +15,27 @@
  */
 package li.strolch.model.timedstate;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
+
 import li.strolch.model.timevalue.ITimeValue;
 import li.strolch.model.timevalue.IValueChange;
 import li.strolch.model.timevalue.impl.FloatValue;
 import li.strolch.model.timevalue.impl.ValueChange;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class TimeStateTest {
 
-	private ITimedState<FloatValue> state = new TimedState<>();
+	private final ITimedState<FloatValue> state = new TimedState<>();
 
-	final FloatValue expectedValue1 = new FloatValue(Double.valueOf(100D));
-	final FloatValue expectedValue2 = new FloatValue(Double.valueOf(200D));
+	final FloatValue expectedValue1 = new FloatValue(100D);
+	final FloatValue expectedValue2 = new FloatValue(200D);
 
-	final Long t0 = Long.valueOf(0);
-	final Long t10 = Long.valueOf(10);
-	final Long t20 = Long.valueOf(20);
-	final Long t30 = Long.valueOf(30);
-	final Long t100 = Long.valueOf(100);
+	final Long t0 = 0L;
+	final Long t10 = 10L;
+	final Long t20 = 20L;
+	final Long t30 = 30L;
+	final Long t100 = 100L;
 
 	@Before
 	public void before() {
@@ -50,14 +48,14 @@ public class TimeStateTest {
 
 		final ITimeValue<FloatValue> stateAt11 = this.state.getStateAt(11L);
 		assertNotNull(stateAt11);
-		assertEquals(true, stateAt11.getValue().matches(this.expectedValue1));
+		assertTrue(stateAt11.getValue().matches(this.expectedValue1));
 
 		final IValueChange<FloatValue> change2 = new ValueChange<>(this.t30, this.expectedValue1);
 		this.state.applyChange(change2, true);
 
 		final ITimeValue<FloatValue> stateAt31 = this.state.getStateAt(31L);
 		assertNotNull(stateAt31);
-		assertEquals(true, stateAt31.getValue().matches(this.expectedValue2));
+		assertTrue(stateAt31.getValue().matches(this.expectedValue2));
 	}
 
 	@Test
