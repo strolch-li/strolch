ef0a38db3003a9a6e51e4d2aaa47c378cba1a082
Robert von Burg <eitch@eitchnet.ch>
2022-11-21 12:11:34 +0100

    [Fix] Fixed WriteRolesFileHelperTest not being idempotent

--- li.strolch.privilege/src/test/java/li/strolch/privilege/test/WriteRolesFileHelperTest.java
+++ li.strolch.privilege/src/test/java/li/strolch/privilege/test/WriteRolesFileHelperTest.java
@@ -1,7 +1,6 @@
 package li.strolch.privilege.test;
 
 import static li.strolch.privilege.test.XmlTest.SRC_TEST;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -17,7 +16,8 @@ public class WriteRolesFileHelperTest {
 		String src = SRC_TEST + "PrivilegeRoles.xml";
 		String dst = "target/WriteRolesFileHelperTest_roles.xml";
 
-		assertFalse(new File(dst).exists());
+		if (new File(dst).exists() && !new File(dst).delete())
+			throw new IllegalStateException("Could not delete file " + dst);
 
 		WriteRolesFileHelper.main(new String[] { src, dst });
 
