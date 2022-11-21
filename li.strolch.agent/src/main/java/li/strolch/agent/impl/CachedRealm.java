1eef4b874619abf48b14f60b906f5706c333bb05
Robert von Burg <eitch@eitchnet.ch>
2022-11-21 12:12:05 +0100

    [New] Implemented async parallel loading from pageable persistence layers

--- li.strolch.agent/src/main/java/li/strolch/agent/impl/CachedRealm.java
+++ li.strolch.agent/src/main/java/li/strolch/agent/impl/CachedRealm.java
@@ -15,20 +15,13 @@
  */
 package li.strolch.agent.impl;
 
-import java.text.MessageFormat;
-import java.util.List;
-import java.util.Set;
-
 import li.strolch.agent.api.*;
-import li.strolch.model.Order;
-import li.strolch.model.Resource;
-import li.strolch.model.activity.Activity;
-import li.strolch.persistence.api.*;
+import li.strolch.persistence.api.PersistenceHandler;
+import li.strolch.persistence.api.StrolchTransaction;
 import li.strolch.privilege.model.Certificate;
 import li.strolch.privilege.model.PrivilegeContext;
 import li.strolch.runtime.configuration.ComponentConfiguration;
 import li.strolch.utils.dbc.DBC;
-import li.strolch.utils.helper.StringHelper;
 
 /**
  * @author Robert von Burg <eitch@eitchnet.ch>
@@ -63,17 +56,17 @@ public class CachedRealm extends InternalStrolchRealm {
 	}
 
 	@Override
-	public ResourceMap getResourceMap() {
+	public CachedResourceMap getResourceMap() {
 		return this.resourceMap;
 	}
 
 	@Override
-	public OrderMap getOrderMap() {
+	public CachedOrderMap getOrderMap() {
 		return this.orderMap;
 	}
 
 	@Override
-	public ActivityMap getActivityMap() {
+	public CachedActivityMap getActivityMap() {
 		return this.activityMap;
 	}
 
@@ -100,69 +93,7 @@ public class CachedRealm extends InternalStrolchRealm {
 	@Override
 	public void start(PrivilegeContext privilegeContext) {
 		super.start(privilegeContext);
-
-		long start = System.nanoTime();
-		int nrOfOrders = 0;
-		int nrOfResources = 0;
-		int nrOfActivities = 0;
-
-		logger.info(MessageFormat.format("Loading Model from Database for realm {0}...", getRealm())); //$NON-NLS-1$
-
-		try (StrolchTransaction tx = openTx(privilegeContext.getCertificate(), "strolch_boot", false)) {
-			ResourceDao resourceDao = tx.getPersistenceHandler().getResourceDao(tx);
-			logger.info("Reading " + resourceDao.querySize() + " Resources from DB...");
-			Set<String> resourceTypes = resourceDao.queryTypes();
-			for (String type : resourceTypes) {
-				logger.info("Reading " + resourceDao.querySize(type) + " Resources of type " + type + " from DB...");
-				List<Resource> resources = resourceDao.queryAll(type);
-				for (Resource resource : resources) {
-					this.resourceMap.insert(resource);
-					nrOfResources++;
-				}
-			}
-
-			tx.commitOnClose();
-		}
-
-		try (StrolchTransaction tx = openTx(privilegeContext.getCertificate(), "strolch_boot", false)) {
-			OrderDao orderDao = tx.getPersistenceHandler().getOrderDao(tx);
-			logger.info("Reading " + orderDao.querySize() + " Orders from DB...");
-			Set<String> orderTypes = orderDao.queryTypes();
-			for (String type : orderTypes) {
-				logger.info("Reading " + orderDao.querySize(type) + " Orders of type " + type + " from DB...");
-				List<Order> orders = orderDao.queryAll(type);
-				for (Order order : orders) {
-					this.orderMap.insert(order);
-					nrOfOrders++;
-				}
-			}
-
-			tx.commitOnClose();
-		}
-
-		try (StrolchTransaction tx = openTx(privilegeContext.getCertificate(), "strolch_boot", false)) {
-			ActivityDao activityDao = tx.getPersistenceHandler().getActivityDao(tx);
-			logger.info("Reading " + activityDao.querySize() + " Activities from DB...");
-			Set<String> activityTypes = activityDao.queryTypes();
-			for (String type : activityTypes) {
-				logger.info("Reading " + activityDao.querySize(type) + " Activities of type " + type + " from DB...");
-				List<Activity> activities = activityDao.queryAll(type);
-				for (Activity activity : activities) {
-					this.activityMap.insert(activity);
-					nrOfActivities++;
-				}
-			}
-
-			tx.commitOnClose();
-		}
-
-		long duration = System.nanoTime() - start;
-		String durationS = StringHelper.formatNanoDuration(duration);
-		logger.info(MessageFormat
-				.format("Loading Model from Database for realm {0} took {1}.", getRealm(), durationS)); //$NON-NLS-1$
-		logger.info(MessageFormat.format("Loaded {0} Orders", nrOfOrders)); //$NON-NLS-1$
-		logger.info(MessageFormat.format("Loaded {0} Resources", nrOfResources)); //$NON-NLS-1$
-		logger.info(MessageFormat.format("Loaded {0} Activities", nrOfActivities)); //$NON-NLS-1$
+		new CachedRealmLoader(this, this.persistenceHandler, privilegeContext).load(getRealm());
 	}
 
 	@Override
---
+++ li.strolch.agent/src/main/java/li/strolch/agent/impl/CachedRealmLoader.java
@@ -0,0 +1,183 @@
+package li.strolch.agent.impl;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.text.MessageFormat;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import li.strolch.model.StrolchRootElement;
+import li.strolch.persistence.api.*;
+import li.strolch.privilege.model.Certificate;
+import li.strolch.privilege.model.PrivilegeContext;
+import li.strolch.utils.helper.StringHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CachedRealmLoader {
+
+	private static final Logger logger = LoggerFactory.getLogger(CachedRealmLoader.class);
+	public static final int PAGE_SIZE = 100;
+
+	private final CachedRealm realm;
+	private final PersistenceHandler persistenceHandler;
+	private final PrivilegeContext privilegeContext;
+
+	private final AtomicInteger nrOfOrders;
+	private final AtomicInteger nrOfResources;
+	private final AtomicInteger nrOfActivities;
+
+	public CachedRealmLoader(CachedRealm realm, PersistenceHandler persistenceHandler,
+			PrivilegeContext privilegeContext) {
+		this.realm = realm;
+		this.persistenceHandler = persistenceHandler;
+		this.privilegeContext = privilegeContext;
+		this.nrOfOrders = new AtomicInteger();
+		this.nrOfResources = new AtomicInteger();
+		this.nrOfActivities = new AtomicInteger();
+	}
+
+	public void load(String realm) {
+		long start = System.nanoTime();
+		logger.info(MessageFormat.format("Loading Model from Database for realm {0}...", realm));
+
+		if (this.persistenceHandler.supportsPaging()) {
+			loadPagingAsync();
+		} else {
+			loadResources();
+			loadOrders();
+			loadActivities();
+		}
+
+		long duration = System.nanoTime() - start;
+		String durationS = StringHelper.formatNanoDuration(duration);
+		logger.info(MessageFormat.format("Loading Model from Database for realm {0} took {1}.", realm, durationS));
+		logger.info(MessageFormat.format("Loaded {0} Orders", this.nrOfOrders));
+		logger.info(MessageFormat.format("Loaded {0} Resources", this.nrOfResources));
+		logger.info(MessageFormat.format("Loaded {0} Activities", this.nrOfActivities));
+	}
+
+	private void loadActivities() {
+		try (StrolchTransaction tx = this.realm.openTx(getCert(), "strolch_boot", false)) {
+			ActivityDao activityDao = this.persistenceHandler.getActivityDao(tx);
+			load("Activities", this.nrOfActivities, activityDao, this.realm.getActivityMap());
+			tx.commitOnClose();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new IllegalStateException("Failed to load activities!", e);
+		}
+	}
+
+	private void loadOrders() {
+		try (StrolchTransaction tx = this.realm.openTx(getCert(), "strolch_boot", false)) {
+			OrderDao orderDao = this.persistenceHandler.getOrderDao(tx);
+			load("Orders", this.nrOfOrders, orderDao, this.realm.getOrderMap());
+			tx.commitOnClose();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new IllegalStateException("Failed to load orders!", e);
+		}
+	}
+
+	private void loadResources() {
+		try (StrolchTransaction tx = this.realm.openTx(getCert(), "strolch_boot", false)) {
+			ResourceDao resourceDao = this.persistenceHandler.getResourceDao(tx);
+			load("Resources", this.nrOfResources, resourceDao, this.realm.getResourceMap());
+			tx.commitOnClose();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new IllegalStateException("Failed to load resources!", e);
+		}
+	}
+
+	private <T extends StrolchRootElement> void load(String context, AtomicInteger counter, StrolchDao<T> dao,
+			CachedElementMap<T> elementMap) throws ExecutionException, InterruptedException {
+
+		logger.info("Loading " + dao.querySize() + " " + context + " from DB...");
+		Set<String> types = dao.queryTypes();
+		for (String type : types) {
+			long sizeOfType = dao.querySize(type);
+			logger.info("Loading " + sizeOfType + " " + context + " of type " + type + " from DB...");
+
+			List<T> elements = dao.queryAll(type);
+			elements.forEach(elementMap::insert);
+			counter.addAndGet(elements.size());
+		}
+	}
+
+	private void loadPagingAsync() {
+		loadElementsPagingAsync("Resources", this.persistenceHandler::getResourceDao, this.realm::getResourceMap,
+				this.nrOfResources);
+		loadElementsPagingAsync("Orders", this.persistenceHandler::getOrderDao, this.realm::getOrderMap,
+				this.nrOfOrders);
+		loadElementsPagingAsync("Activities", this.persistenceHandler::getActivityDao, this.realm::getActivityMap,
+				this.nrOfActivities);
+	}
+
+	private <T extends StrolchRootElement> void loadElementsPagingAsync(String context,
+			Function<StrolchTransaction, StrolchDao<T>> daoSupplier, Supplier<CachedElementMap<T>> elementMapSupplier,
+			AtomicInteger counter) {
+
+		long start = System.nanoTime();
+
+		Map<String, Long> sizeByTypes = getSizesByType(daoSupplier);
+		CachedElementMap<T> elementMap = elementMapSupplier.get();
+
+		long nrOfElements = sizeByTypes.values().stream().mapToLong(Long::longValue).sum();
+		logger.info("Loading " + nrOfElements + " " + context + " from DB...");
+
+		List<CompletableFuture<Void>> tasks = new ArrayList<>();
+		sizeByTypes.forEach((type, size) -> {
+			logger.info("Loading " + size + " " + context + " of type " + type + " from DB async in parallel...");
+			long position = 0;
+			while (position < size) {
+				long p = position;
+
+				tasks.add(CompletableFuture.runAsync(() -> {
+					try (StrolchTransaction tx = this.realm.openTx(getCert(), "strolch_boot", true)
+							.silentThreshold(10, SECONDS)
+							.suppressUpdates()) {
+						List<T> elements = daoSupplier.apply(tx).queryAll(PAGE_SIZE, p, type);
+						elements.forEach(elementMap::insert);
+						counter.addAndGet(elements.size());
+					}
+				}));
+
+				position += PAGE_SIZE;
+			}
+		});
+
+		Throwable failureEx;
+		try {
+			failureEx = CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0]))
+					.handle((unused, throwable) -> throwable)
+					.get();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new IllegalStateException("Failed to load " + context, e);
+		}
+
+		if (failureEx != null)
+			throw new IllegalStateException("Failed to load " + context, failureEx);
+
+		String durationS = StringHelper.formatNanoDuration(System.nanoTime() - start);
+		logger.info(MessageFormat.format("Loading of {0} {1} took {2}.", nrOfElements, context, durationS));
+	}
+
+	private <T extends StrolchRootElement> Map<String, Long> getSizesByType(
+			Function<StrolchTransaction, StrolchDao<T>> daoSupplier) {
+		Map<String, Long> sizeByTypes = new HashMap<>();
+		try (StrolchTransaction tx = this.realm.openTx(getCert(), "strolch_boot", true).silentThreshold(10, SECONDS)) {
+			StrolchDao<T> dao = daoSupplier.apply(tx);
+			Set<String> types = dao.queryTypes();
+			for (String type : types) {
+				sizeByTypes.put(type, dao.querySize(type));
+			}
+		}
+		return sizeByTypes;
+	}
+
+	private Certificate getCert() {
+		return this.privilegeContext.getCertificate();
+	}
+}
