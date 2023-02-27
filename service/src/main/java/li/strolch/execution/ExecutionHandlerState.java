5230e808797bb71a951986e9cf0e391251b8482c
Robert von Burg <eitch@eitchnet.ch>
2023-02-27 10:49:59 +0100

    [Minor] Added ExecutionHandlerState methods

--- service/src/main/java/li/strolch/execution/ExecutionHandlerState.java
+++ service/src/main/java/li/strolch/execution/ExecutionHandlerState.java
@@ -4,4 +4,16 @@ public enum ExecutionHandlerState {
 	Running,
 	HaltNew,
 	Paused;
+
+	public boolean isRunning() {
+		return this == Running;
+	}
+
+	public boolean isPaused() {
+		return this == Paused;
+	}
+
+	public boolean isHaltNew() {
+		return this == HaltNew;
+	}
 }
