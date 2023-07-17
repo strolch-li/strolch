c9e771a9dc77a58df29b20ae6a2fe88b0a9552f3
Robert von Burg <eitch@eitchnet.ch>
2023-07-17 17:28:50 +0200

    [Minor] Code cleanup

--- model/src/main/java/li/strolch/model/ModelHelper.java
+++ model/src/main/java/li/strolch/model/ModelHelper.java
@@ -1,9 +1,9 @@
 package li.strolch.model;
 
-import static li.strolch.model.StrolchModelConstants.BAG_PARAMETERS;
-import static li.strolch.model.StrolchModelConstants.BAG_RELATIONS;
-
+import li.strolch.model.parameter.Parameter;
 import li.strolch.model.parameter.StringParameter;
+
+import static li.strolch.model.StrolchModelConstants.*;
 
 /**
  * Helper class to perform often performed model commands
@@ -27,5 +27,16 @@ public class ModelHelper {
 				dst.setString(bagId, paramId, existingBillingIdP.getValue());
 			}
 		}
+	}
+
+	public static void setInterpretationAndUom(Parameter<?> relationP, String objectType, String elementType) {
+		switch (objectType) {
+			case Tags.RESOURCE -> relationP.setInterpretation(INTERPRETATION_RESOURCE_REF);
+			case Tags.ORDER -> relationP.setInterpretation(INTERPRETATION_ORDER_REF);
+			case Tags.ACTIVITY -> relationP.setInterpretation(INTERPRETATION_ACTIVITY_REF);
+			default -> throw new IllegalStateException("Unexpected value: " + objectType);
+		}
+
+		relationP.setUom(elementType);
 	}
 }
