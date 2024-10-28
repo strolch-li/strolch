955282d26180b03bed8284f5af1bed7cd59ae128
Robert von Burg <eitch@eitchnet.ch>
2024-10-28 07:42:59 +0100

    [Minor] reformat code

--- strolch-model/src/main/java/li/strolch/model/timevalue/impl/TimeVariable.java
+++ strolch-model/src/main/java/li/strolch/model/timevalue/impl/TimeVariable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Martin Smock <smock.martin@gmail.com>
+ * Copyright (c) 2013-2024 Robert von Burg <eitch@eitchnet.ch>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- strolch-service/src/main/java/li/strolch/service/AddOrUpdateOrderService.java
+++ strolch-service/src/main/java/li/strolch/service/AddOrUpdateOrderService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Reto Breitenmoser <reto.breitenmoser@gmail.com>
+ * Copyright (c) 2017-2024 Robert von Burg <eitch@eitchnet.ch>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
