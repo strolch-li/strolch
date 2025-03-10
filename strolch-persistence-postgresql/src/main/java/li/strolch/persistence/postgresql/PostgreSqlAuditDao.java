71645a22605c3b80957092560fd2a3bc89c7bc9c
Robert von Burg <eitch@eitchnet.ch>
2025-03-10 10:02:38 +0100

    [New] Add support for 'additional_data' field in audits
    Extended the PostgreSqlAuditDao to handle an 'additional_data' JSON field for audit entries. This includes updates to SQL queries, setting and retrieving the field, and parsing JSON values for serialization and deserialization.

--- strolch-persistence-postgresql/src/main/java/li/strolch/persistence/postgresql/PostgreSqlAuditDao.java
+++ strolch-persistence-postgresql/src/main/java/li/strolch/persistence/postgresql/PostgreSqlAuditDao.java
@@ -15,11 +15,13 @@
  */
 package li.strolch.persistence.postgresql;
 
+import com.google.gson.JsonParser;
 import li.strolch.model.audit.AccessType;
 import li.strolch.model.audit.Audit;
 import li.strolch.persistence.api.AuditDao;
 import li.strolch.persistence.api.StrolchPersistenceException;
 import li.strolch.utils.collections.DateRange;
+import org.postgresql.util.PGobject;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -47,8 +49,9 @@ public class PostgreSqlAuditDao implements AuditDao {
 	public static final String LASTNAME = "lastname";
 	public static final String FIRSTNAME = "firstname";
 	public static final String USERNAME = "username";
+	public static final String ADDITIONAL_DATA = "additional_data";
 	public static final String FIELDS = commaSeparated(ID, USERNAME, FIRSTNAME, LASTNAME, DATE, ELEMENT_TYPE,
-			ELEMENT_SUB_TYPE, ELEMENT_ACCESSED, NEW_VERSION, ACTION, ACCESS_TYPE);
+			ELEMENT_SUB_TYPE, ELEMENT_ACCESSED, NEW_VERSION, ACTION, ACCESS_TYPE, ADDITIONAL_DATA);
 	public static final String TABLE_NAME = "audits";
 
 	private static final String hasElementSql = "select count(*) from audits where element_type = ? and id = ?";
@@ -62,9 +65,9 @@ public class PostgreSqlAuditDao implements AuditDao {
 			+ " from audits where element_type = ? and date between ? and ?";
 	private static final String insertSql = "insert into audits ("
 			+ FIELDS
-			+ ") values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?::access_type)";
+			+ ") values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?::access_type, ?)";
 	private static final String updateSql
-			= "update audits set id = ?, username = ?, firstname = ?, lastname = ?, date = ?, element_type = ?, element_sub_type = ?, element_accessed = ?, new_version = ?, action = ?, access_type = ?::access_type where id = ?";
+			= "update audits set id = ?, username = ?, firstname = ?, lastname = ?, date = ?, element_type = ?, element_sub_type = ?, element_accessed = ?, new_version = ?, action = ?, access_type = ?::access_type, additional_data = ? where id = ?";
 	private static final String removeSql = "delete from audits where id = ?";
 	private static final String removeAllSql = "delete from audits where element_type = ? and date between ? and ?";
 
@@ -205,7 +208,7 @@ public class PostgreSqlAuditDao implements AuditDao {
 			int count = preparedStatement.executeUpdate();
 			if (count != 1) {
 				throw new StrolchPersistenceException(
-						MessageFormat.format("Expected to insert 1 record, but inserted {0} for audit {2}", count,
+						MessageFormat.format("Expected to insert 1 record, but inserted {0} for audit {1}", count,
 								audit.getId()));
 			}
 
@@ -227,12 +230,12 @@ public class PostgreSqlAuditDao implements AuditDao {
 		try (PreparedStatement preparedStatement = this.tx.getConnection().prepareStatement(updateSql)) {
 
 			setAuditFields(audit, preparedStatement);
-			preparedStatement.setLong(12, audit.getId());
+			preparedStatement.setLong(13, audit.getId());
 
 			int count = preparedStatement.executeUpdate();
 			if (count != 1) {
 				throw new StrolchPersistenceException(
-						MessageFormat.format("Expected to update 1 record, but updated {0} for audit {2}", count,
+						MessageFormat.format("Expected to update 1 record, but updated {0} for audit {1}", count,
 								audit.getId()));
 			}
 
@@ -264,7 +267,7 @@ public class PostgreSqlAuditDao implements AuditDao {
 
 		} catch (SQLException e) {
 			throw new StrolchPersistenceException(
-					MessageFormat.format("Failed to remove {0} due to {2}", audit.getId(), e.getLocalizedMessage()), e);
+					MessageFormat.format("Failed to remove {0} due to {1}", audit.getId(), e.getLocalizedMessage()), e);
 		}
 	}
 
@@ -304,6 +307,7 @@ public class PostgreSqlAuditDao implements AuditDao {
 		// 9  new_version = ?, 
 		// 10 action = ?, 
 		// 11 access_type = ?::access_type
+		// 12 additional_data
 
 		ps.setLong(1, audit.getId());
 		ps.setString(2, audit.getUsername());
@@ -321,6 +325,15 @@ public class PostgreSqlAuditDao implements AuditDao {
 
 		ps.setString(10, audit.getAction());
 		ps.setString(11, audit.getAccessType().name());
+
+		if (audit.getAdditionalData() == null) {
+			ps.setObject(12, null);
+		} else {
+			PGobject pGobject = new PGobject();
+			pGobject.setType("json");
+			pGobject.setValue(audit.getAdditionalData().toString());
+			ps.setObject(12, pGobject);
+		}
 	}
 
 	private Audit auditFrom(ResultSet resultSet) throws SQLException {
@@ -337,6 +350,13 @@ public class PostgreSqlAuditDao implements AuditDao {
 		audit.setNewVersion(resultSet.getTimestamp(9));
 		audit.setAction(resultSet.getString(10));
 		audit.setAccessType(AccessType.valueOf(resultSet.getString(11)));
+
+		PGobject pGobject = (PGobject) resultSet.getObject(ADDITIONAL_DATA);
+		if (pGobject != null) {
+			String json = pGobject.getValue();
+			if (json != null)
+				audit.setAdditionalData(JsonParser.parseString(json));
+		}
 		return audit;
 	}
 }
