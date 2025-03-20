c3ea33164cef32455dd0f30f64a74cf927c5dfb5
Robert von Burg <eitch@eitchnet.ch>
2025-03-20 13:14:17 +0100

    [New] Enhance REST exception logging with request details
    Added HTTP method, URL, and remote IP to REST exception logs for better traceability. Updated `StrolchRestfulExceptionMapper` to include this information in the operations log and adapted related property messages accordingly.

--- strolch-web-rest/src/main/java/li/strolch/rest/StrolchRestfulExceptionMapper.java
+++ strolch-web-rest/src/main/java/li/strolch/rest/StrolchRestfulExceptionMapper.java
@@ -15,12 +15,15 @@
  */
 package li.strolch.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import li.strolch.agent.api.StrolchAgent;
 import li.strolch.exception.StrolchAccessDeniedException;
 import li.strolch.exception.StrolchNotAuthenticatedException;
 import li.strolch.handler.operationslog.OperationsLog;
@@ -29,14 +32,16 @@ import li.strolch.model.log.LogMessage;
 import li.strolch.model.log.LogMessageState;
 import li.strolch.model.log.LogSeverity;
 import li.strolch.privilege.base.AccessDeniedException;
-import li.strolch.privilege.model.CertificateThreadLocal;
 import li.strolch.rest.helper.ResponseUtil;
+import li.strolch.rest.helper.RestfulHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ResourceBundle;
 
 import static li.strolch.model.Tags.AGENT;
+import static li.strolch.privilege.model.CertificateThreadLocal.getCert;
+import static li.strolch.privilege.model.CertificateThreadLocal.hasCert;
 import static li.strolch.utils.helper.ExceptionHelper.hasCause;
 
 @Provider
@@ -44,26 +49,31 @@ public class StrolchRestfulExceptionMapper implements ExceptionMapper<Exception>
 
 	private static final Logger logger = LoggerFactory.getLogger(StrolchRestfulExceptionMapper.class);
 
+	@Context
+	private HttpServletRequest request;
+
 	@Override
 	public Response toResponse(Exception ex) {
 
 		logger.error("Handling exception {}", ex.getClass(), ex);
 
-		RestfulStrolchComponent instance = RestfulStrolchComponent.getInstance();
+		StrolchAgent agent = RestfulStrolchComponent.getInstance().getAgent();
 		boolean isNotAccessDeniedException = !hasCause(ex, AccessDeniedException.class) && !hasCause(ex,
 				StrolchAccessDeniedException.class);
-		if (isNotAccessDeniedException && instance.hasComponent(OperationsLog.class)) {
+		// access denied exceptions have already been handled
+		if (isNotAccessDeniedException && agent.hasComponent(OperationsLog.class)) {
 			try {
-				String username = CertificateThreadLocal.hasCert() ? CertificateThreadLocal.getCert().getUsername() :
-						"anonymous";
-				String realm = instance.getAgent().getRealmNames().iterator().next();
-				OperationsLog operationsLog = instance.getComponent(OperationsLog.class);
+				String username = hasCert() ? getCert().getUsername() : "anonymous";
+				String realm = agent.getRealmNames().iterator().next();
+				OperationsLog operationsLog = agent.getComponent(OperationsLog.class);
 				operationsLog.addMessage(new LogMessage(realm, username,
 						Locator.valueOf(AGENT, RestfulStrolchComponent.class.getSimpleName(),
 								ex.getClass().getSimpleName()), LogSeverity.Exception, LogMessageState.Information,
 						ResourceBundle.getBundle("strolch-agent"), "web.rest.exception")
-						.withException(ex)
-						.value("exception", ex));
+						.value("exception", ex)
+						.value("method", this.request == null ? "??" : this.request.getMethod())
+						.value("url", this.request == null ? "??" : this.request.getRequestURL().toString())
+						.value("remoteIp", this.request == null ? "??" : RestfulHelper.getRemoteIp(this.request)));
 			} catch (Exception e) {
 				logger.error("Failed to add log message for exception!", e);
 			}
