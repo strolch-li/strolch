57325da4f6c8fd09b69036298bc1e8b4a27f6ca2
Robert von Burg <eitch@eitchnet.ch>
2025-05-05 11:00:24 +0200

    [New] Add notification type feature to notifications model
    Introduced a `NotificationType` enum and integrated it into the notification creation process. Added a new `PARAM_NOTIFICATION_TYPE` field to resources and ensured it defaults to `Information` when not explicitly provided. Extended REST endpoints to include the notification type in responses.

--- strolch-service/src/main/java/li/strolch/service/notifications/CreateNotificationService.java
+++ strolch-service/src/main/java/li/strolch/service/notifications/CreateNotificationService.java
@@ -18,6 +18,7 @@ package li.strolch.service.notifications;
 
 import com.google.gson.JsonObject;
 import li.strolch.agent.api.StrolchAgent;
+import li.strolch.model.NotificationType;
 import li.strolch.model.ParameterBag;
 import li.strolch.model.Resource;
 import li.strolch.model.builder.ResourceBuilder;
@@ -70,6 +71,11 @@ public class CreateNotificationService extends AbstractService<JsonServiceArgume
 			Set<String> supportedLanguages) {
 		Resource notification = newNotification();
 		PrivilegeContext ctx = tx.getPrivilegeContext();
+
+		if (jsonObject.has(PARAM_NOTIFICATION_TYPE))
+			notification.setString(PARAM_NOTIFICATION_TYPE, jsonObject.get(PARAM_NOTIFICATION_TYPE).getAsString());
+		else
+			notification.setString(PARAM_NOTIFICATION_TYPE, NotificationType.Information);
 
 		JsonObject visibilityJ = jsonObject.get(BAG_VISIBILITY).getAsJsonObject();
 		ParameterBag visibility = notification.getParameterBag(BAG_VISIBILITY);
@@ -126,6 +132,8 @@ public class CreateNotificationService extends AbstractService<JsonServiceArgume
 
 	public static Resource newNotification() {
 		ResourceBuilder notificationBuilder = new ResourceBuilder(TYPE_NOTIFICATION, TYPE_NOTIFICATION)
+
+				.defaultBag().string(PARAM_NOTIFICATION_TYPE).end().endBag()
 
 				.bag(BAG_VISIBILITY, TYPE_VISIBILITY)
 
