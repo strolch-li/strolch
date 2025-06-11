a236bb40e559cdc2fae1b168b320c3d1d11acb23
Robert von Burg <eitch@eitchnet.ch>
2025-06-11 09:37:52 +0200

    [Minor] Log PostgreSQL version during schema version check
    Added a log statement to display the PostgreSQL version during the database schema version check. This enhances traceability and provides context regarding the database environment during runtime.

--- strolch-utils/src/main/java/li/strolch/db/DbSchemaVersionCheck.java
+++ strolch-utils/src/main/java/li/strolch/db/DbSchemaVersionCheck.java
@@ -107,6 +107,11 @@ public class DbSchemaVersionCheck {
 		// get current version
 		Version currentVersion;
 		try (Connection con = ds.getConnection()) {
+
+			DatabaseMetaData metaData = con.getMetaData();
+			String version = metaData.getDatabaseProductVersion();
+			logger.info("Connected to PostgreSQL {}", version);
+
 			currentVersion = getCurrentVersion(con, this.app);
 			con.commit();
 		} catch (SQLException e) {
