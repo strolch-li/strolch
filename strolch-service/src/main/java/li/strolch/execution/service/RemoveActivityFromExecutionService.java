61cc98c33e0606389614978df9f54c187ae4f90e
Robert von Burg <eitch@eitchnet.ch>
2025-06-24 10:27:14 +0200

    [Fix] Refactor realm assignment in service
    Replaced direct realm assignment logic with `getArgOrUserRealm` method for clarity and to avoid duplication. This improves maintainability by centralizing the logic for determining the realm.

--- strolch-service/src/main/java/li/strolch/execution/service/RemoveActivityFromExecutionService.java
+++ strolch-service/src/main/java/li/strolch/execution/service/RemoveActivityFromExecutionService.java
@@ -17,12 +17,10 @@
 package li.strolch.execution.service;
 
 import li.strolch.execution.ExecutionHandler;
-import li.strolch.runtime.StrolchConstants;
 import li.strolch.service.LocatorArgument;
 import li.strolch.service.api.AbstractService;
 import li.strolch.service.api.ServiceResult;
 import li.strolch.service.api.ServiceResultState;
-import li.strolch.utils.helper.StringHelper;
 
 public class RemoveActivityFromExecutionService extends AbstractService<LocatorArgument, ServiceResult> {
 
@@ -39,8 +37,7 @@ public class RemoveActivityFromExecutionService extends AbstractService<LocatorA
 	@Override
 	protected ServiceResult internalDoService(LocatorArgument arg) {
 
-		String realm = StringHelper.isEmpty(arg.realm) ? StrolchConstants.DEFAULT_REALM : arg.realm;
-
+		String realm = getArgOrUserRealm(arg);
 		ExecutionHandler executionHandler = getContainer().getComponent(ExecutionHandler.class);
 		executionHandler.removeFromExecution(realm, arg.locator);
 
