5017a7195241188cd2ef664c40473b30031379e4
Robert von Burg <eitch@eitchnet.ch>
2025-07-11 11:32:41 +0200

    [Fix] Add root cause toggle to `toResponse` methods
    Updated `toResponse` methods to include the `onlyRootCause` parameter, allowing more focused error responses. Enhanced both `ServiceResultResponse` and `JsonServiceResultResponse` to support this toggle, offering greater control over displaying exception details.

--- strolch-web-rest/src/main/java/li/strolch/rest/helper/ResponseUtil.java
+++ strolch-web-rest/src/main/java/li/strolch/rest/helper/ResponseUtil.java
@@ -134,24 +134,25 @@ public class ResponseUtil {
 	}
 
 	public static Response toResponse(JsonServiceResult svcResult) {
-		return toResponse(svcResult, true);
+		return toResponse(svcResult, true, false);
 	}
 
-	public static Response toResponse(JsonServiceResult svcResult, boolean withStackTrace) {
+	public static Response toResponse(JsonServiceResult svcResult, boolean withStackTrace, boolean onlyRootCause) {
 		if (svcResult.isOk())
 			return Response
-					.ok(new JsonServiceResultResponse(svcResult, withStackTrace).toJson(), APPLICATION_JSON)
+					.ok(new JsonServiceResultResponse(svcResult, withStackTrace, onlyRootCause).toJson(),
+							APPLICATION_JSON)
 					.build();
 		return toResponse((ServiceResult) svcResult);
 	}
 
 	public static Response toResponse(ServiceResult svcResult) {
-		return toResponse(svcResult, true);
+		return toResponse(svcResult, true, true);
 	}
 
-	public static Response toResponse(ServiceResult svcResult, boolean withStackTrace) {
+	public static Response toResponse(ServiceResult svcResult, boolean withStackTrace, boolean onlyRootCause) {
 		Throwable t = svcResult.getThrowable();
-		ServiceResultResponse response = new ServiceResultResponse(svcResult, withStackTrace);
+		ServiceResultResponse response = new ServiceResultResponse(svcResult, withStackTrace, onlyRootCause);
 		String json = new Gson().toJson(response);
 
 		if (svcResult.isOk())
